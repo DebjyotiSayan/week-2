c85f3c8d279eb57c98eb2165b30da2735e9e30a2
DebjyotiSayan <143388775+DebjyotiSayan@users.noreply.github.com>
2023-10-03 16:44:15 +0530

    Add files via upload

---
+++ c_triangle.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   int i,j,s,n;
+   printf("enter the value of n");
+    scanf("%d",&n);
+   for(i=0;i<=n;i++)
+   {
+   	for(s=0;s<n-j;s++)
+   	printf(" ");
+   	for(j=0;j<=i+1;j++)
+   	printf("*");
+   	printf("\n");
+   }
+   	return 0;
+   }	
+		
+
